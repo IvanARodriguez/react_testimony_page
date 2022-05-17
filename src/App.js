ec79062008169034961b45898f19c281a95728fd
Ivan Rodriguez <ivancalvo91@gmail.com>
2022-05-17 03:19:34 -0500

    react testimony finished

--- src/App.js
+++ src/App.js
@@ -5,8 +5,31 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h1>This is what our CEOs have to say</h1>
-        <Testimony />
+        <h1>The testimony Hub</h1>
+        <Testimony 
+          name="Jhon Doe"
+          imageURL="jhondoe"
+          country="USA"
+          testimony="When I learned to program I felt that I discovered super power, however, my learning journey has been full of ups and downs because there are so many resources and learning path to take that sometimes I found myself trying to learn many different technologies and library at the same time. Until I met Ivan who coach me to learn and master one thing at a time."
+          profession="Software Enginner"
+          company="Amazon Prime"
+        />
+        <Testimony 
+          name="Blake Smith"
+          imageURL="Blake"
+          country="Puerto Rico"
+          testimony="I take my job seriously and my team are like a family, if they have a good performance I do too. that is why I always think about them, support them and try to have bonuses and snacks all the time."
+          profession="Team Manager"
+          company="Brown Co."
+        />
+        <Testimony 
+          name="Smith James"
+          imageURL="James"
+          country="Germany"
+          testimony="My job is very impoertan, because a single digit can cause a massive impact in a company's financial aspect. That is why I respect my job, always being early, separating life and work, and being healthy to have an outstanding performance at what I do."
+          profession="Accountant"
+          company="Google Cloud"
+        />
       </div>
     </div>
   );
--- src/components/Testimony.js
+++ src/components/Testimony.js
@@ -2,17 +2,17 @@ import React from 'react';
 import '../stylesheets/Testimony.css'
 
 
- function Testimony(){
+ function Testimony(props){
 	return(
 		<div className='testimony_container'>
 			<img  className='testimony_image'
-			src={require('../images/avatar1.webp')}
+			src={require(`../images/${props.imageURL}.webp`)}
 			alt='CEO picture'
 			/>
 			<div className="text_content_container">
-				<p className='testimony_name'>Jhon Doe in Dallas</p>
-				<p className='testimony_profession'>Softare Enginner at Amazon</p>
-				<p className="testimony_text">"When I learned to program I felt that I discovered super power, however, my learning journey has been full of ups and downs because there are so many resources and learning path to take that sometimes I found myself trying to learn many different technologies and library at the same time. Until I met Ivan who coach me to learn and master one thing at a time."</p>
+				<p className='testimony_name'>{props.name} in {props.country}</p>
+				<p className='testimony_profession'>{props.profession} at {props.company}</p>
+				<p className="testimony_text">{props.testimony}</p>
 			</div>
 		</div>
 	);
